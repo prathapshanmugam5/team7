ce5bc45d78e2906fe85a6f773298e2bb127ad4e6
saran s <147602854+saran13092@users.noreply.github.com>
2024-02-01 17:18:32 +0530

    Delete saran directory

--- saran/Server/src/main/java/com/security/Exception/InvalidUserException.java
+++
@@ -1,9 +0,0 @@
-package com.security.Exception;
-
-public class InvalidUserException extends RuntimeException {
-	
-	public InvalidUserException(String message) {
-        super(message);
-    }
-
-}
--- saran/Server/src/main/java/com/security/entity/UsernameNotFoundException.java
+++
@@ -1,5 +0,0 @@
-package com.security.entity;
-
-public class UsernameNotFoundException {
-
-}
