7e036e3392bfa12542324aaf5b45585a36f0630b
prathap-oasys <prathapshanmugam5@gmail.com>
2024-02-02 17:29:27 +0530

    snakbar

--- Server/src/main/java/com/security/controller/ProductController.java
+++ Server/src/main/java/com/security/controller/ProductController.java
@@ -3,7 +3,6 @@ package com.security.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
